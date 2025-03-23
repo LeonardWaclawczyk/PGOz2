92a612acfcc621b996a802283f523ff483b012fa
Leonard Wacławczyk <leo@172.23.73.12.wi-fi.pjwstk.edu.pl>
2025-03-23 13:33:23 +0100

    Read#2

--- src/cw2/Produkt.java
+++ src/cw2/Produkt.java
@@ -6,45 +6,17 @@ public class Produkt {
     private double cena;
     private int iloscWMagazynie;
 
-    public int getId() {
-        return id;
-    }
+    public int getId() {return id;}
+    public String getNazwa() {return Nazwa;}
+    public String getKategoria() {return Kategoria;}
+    public double getCena() {return cena;}
+    public int getIloscWMagazynie() {return iloscWMagazynie;}
+    public void setId(int id) {this.id = id;}
+    public void setNazwa(String nazwa) {Nazwa = nazwa;}
+    public void setKategoria(String kategoria) {Kategoria = kategoria;}
+    public void setCena(double cena) {this.cena = cena;}
+    public void setIloscWMagazynie(int iloscWMagazynie) {this.iloscWMagazynie = iloscWMagazynie;}
 
-    public String getNazwa() {
-        return Nazwa;
-    }
-
-    public String getKategoria() {
-        return Kategoria;
-    }
-
-    public double getCena() {
-        return cena;
-    }
-
-    public int getIloscWMagazynie() {
-        return iloscWMagazynie;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNazwa(String nazwa) {
-        Nazwa = nazwa;
-    }
-
-    public void setKategoria(String kategoria) {
-        Kategoria = kategoria;
-    }
-
-    public void setCena(double cena) {
-        this.cena = cena;
-    }
-
-    public void setIloscWMagazynie(int iloscWMagazynie) {
-        this.iloscWMagazynie = iloscWMagazynie;
-    }
     public void wyswietlInformacje() {
         System.out.println("Id: " + id);
         System.out.println("Nazwa: " + Nazwa);
